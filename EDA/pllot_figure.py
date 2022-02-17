1d5998fbc5642d4972647b7c0720e1bfbfc8d358
Tomoki <72484185+tomoki-trader@users.noreply.github.com>
2022-02-17 12:13:37 +0900

    Update pllot_figure.py

--- EDA/pllot_figure.py
+++ EDA/pllot_figure.py
@@ -1,7 +1,5 @@
-#one val for hist
+#one argument for hist
 import matplotlib.pyplot as plt
-fig = plt.figure(figsize=(7,7))
-fig, ax = plt.subplots(1,1)
-ax.hist(y, label='test', alpha=0.7, density=False, )
-ax.hist(x, label='train', alpha=0.3, density=False, )
-ax.legend()
+fig = plt.figure(figsize=(14,7))
+ax1 = plt.subplot(1,2,1)
+ax2 = plt.subplot(1,2,2)
